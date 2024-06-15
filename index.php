dcc05774b052a956f23e318c0209d36fb5b82f1f
Albert-pene <nournewapppc@gmail.com>
2024-06-15 18:30:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Welcome to our home</title>
+</head>
+<body>
+    <?php 
+     
+    echo "<nav>";
+        echo "   <button>Download tools</button>";
+        echo "   <button>Get links</button>";
+        echo "   <button>Explore projects</button>";
+        echo "   <button>Send feedback</button>";
+        echo "   <button>Contact Us</button>";
+        echo "   <button>Bug report</button>";
+        echo "   <button>Sign in</button>";
+        echo "   <button>Sign up</button>";
+        echo "</nav>";
+   ?>
+   <img src="creative-3d-render-astronaut-flying-with-rocket-play-video-buttoon-web-development-banner-marketing-material-presentation-online-advertising.jpg">
+</body>
+</html>
